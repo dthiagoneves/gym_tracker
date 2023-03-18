796a3b587327c52bf9c6e382b6e2a9dce8ae6ac0
myprojectsgithub <derp.thiago@gmail.com>
2023-03-18 21:58:24 +0100

    Added buttons to add or remove execises and the labels and entries required to input the gym session information

---
+++ view.py
@@ -0,0 +1,50 @@
+import customtkinter as ctk
+
+
+class View(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Gym Tracker v1.0")
+        self.minsize(400, 300)
+        self.frame = SessionInput(self)
+        self.frame.grid(row=0, column=0, padx=20, pady=20)
+
+
+class SessionInput(ctk.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.frame_elements = []
+
+        self.button_plus = ctk.CTkButton(self, text="+", width=15, height=15, command=self.create_input)
+        self.button_plus.grid(row=0, column=0, padx=0, pady=5)
+
+        self.button_minus = ctk.CTkButton(self, text="-", width=15, height=15, command=self.delete_input)
+        self.button_minus.grid(row=0, column=1, padx=0, pady=5)
+
+        # Create 4 exercise options by default
+        for _index in range(4):
+            self.create_input()
+
+    def create_input(self):
+        _index = len(self.frame_elements)
+        label = ctk.CTkLabel(self, text="Exercise %s:" % str(_index+1))
+        label.grid(row=_index+1, column=0, padx=10, pady=10)
+
+        entry = ctk.CTkEntry(self, width=60, height=15, border_width=1.5, corner_radius=10)
+        entry.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+        entry.grid(row=_index+1, column=1, padx=10, pady=10)
+        # Save label and entry references to frame_elements
+        self.frame_elements.append((label, entry))
+
+    def delete_input(self):
+        if len(self.frame_elements) > 1:
+            self.frame_elements[-1][0].destroy()
+            self.frame_elements[-1][1].destroy()
+            del self.frame_elements[-1]
+
+
+if __name__ == "__main__":
+    view = View()
+    view.mainloop()
